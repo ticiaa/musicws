2bcc199d3fb5639ea76d6eb6b601f348f0e7003d
Letícia <leticialindberght@gmail.com>
2021-06-11 22:40:32 -0300

    Music Commit

--- src/main/java/br/com/tech4music/musicws/model/Musica.java
+++ src/main/java/br/com/tech4music/musicws/model/Musica.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 
 import org.springframework.data.annotation.Id;
